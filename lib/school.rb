c0979968661412d59e0969ac20daf2e171c8742f
Justin Shaw <easyworkemail@gmail.com>
2019-05-01 19:30:53 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,16 @@
 # code here!
 class School
   attr_accessor
-  attr_reader :roster
+  attr_reader :roster, :add_student
+  STUDENTS = []
 
   def initialize(roster)
     @roster = {}
   end
 
-  def add_student
-
+  def add_student=(student, grade)
+    STUDENTS << student
+    @roster[grade] = [STUDENTS]
   end
 
   def grade
@@ -23,3 +25,7 @@ end
  # school = School.new("Bayside High School")
 
  # p school.roster # => {}
+
+# school.add_student("Zach Morris", 9)
+# school.roster
+# => {9 => ["Zach Morris"]}
