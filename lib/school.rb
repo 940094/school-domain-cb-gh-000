32b8d44bda4843a1040ac0c146cdab79a3d36105
Justin Shaw <easyworkemail@gmail.com>
2019-05-01 19:28:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,6 +8,7 @@ class School
   end
 
   def add_student
+
   end
 
   def grade
@@ -17,3 +18,9 @@ class School
   end
 
 end
+
+
+ # school = School.new("Bayside High School")
+
+ # school.roster
+# => {}
