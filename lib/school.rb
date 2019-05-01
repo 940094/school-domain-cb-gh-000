ba2877e1cb30f0fecdcc7598654b8686ec84e7b0
Justin Shaw <easyworkemail@gmail.com>
2019-05-01 19:52:44 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -22,17 +22,22 @@ class School
   end
 
   def sort
+    @roster.each do |grade|  # iterate through each grade
+      p roster[grade].sort  # call the sort method on each grade
+    end
   end
 
 end
 
-describe '#grade' do
-  it 'is able to retreive students from a grade' do
+describe "#sort" do
+  it 'is able to sort the students' do
     @school.add_student("Homer Simpson", 9)
+    @school.add_student("Bart Simpson", 9)
     @school.add_student("Avi Flombaum", 10)
     @school.add_student("Jeff Baird", 10)
     @school.add_student("Blake Johnson", 7)
+    @school.add_student("Jack Bauer", 7)
 
-    expect(@school.grade(10)).to eq(["Avi Flombaum", "Jeff Baird"])
+    # key order does not matter; this is testing that the students in each respective value are in alphabetical order
+    expect(@school.sort).to eq({7 => ["Blake Johnson", "Jack Bauer"], 9 => ["Bart Simpson", "Homer Simpson"], 10 => ["Avi Flombaum", "Jeff Baird"]})
   end
-end
