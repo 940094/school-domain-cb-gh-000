afc830ea6e757ce6bc6e792b04e17cdeae44e4ba
Justin Shaw <easyworkemail@gmail.com>
2019-05-01 18:38:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
 # code here!
+class School
+  def initialize(roster)
+    @roster = roster
+  end 
+
+  def add_student(student)
+    @roaster << student
+  end
+
+  def grade
+  end
+
+  def sort
+  end
+end
